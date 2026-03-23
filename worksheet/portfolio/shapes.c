dba85c3a8d4631364696f70d1ed287de85b92f6e
ELLCQ <lhth0940@leeds.ac.uk>
2026-03-23 16:57:24 +0000

    0

--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -7,6 +7,8 @@
 Point makePoint( float x, float y ) {
     Point new;
     // implementation
+    new.x = 1.0;
+    new.y = 2.0;
     return new;
 }
 
@@ -14,3 +16,8 @@ Point makePoint( float x, float y ) {
 // - start with stubs as above
 // - compile regularly to test syntax
 // - test functions by calling them from main()
+
+Line makeLine( Point p1, Point p2 ){
+    Line l;
+    
+}
