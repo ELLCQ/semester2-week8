764f5124dcbcda3d7d79d898bba011984f1288b5
ELLCQ <lhth0940@leeds.ac.uk>
2026-03-24 16:08:18 +0000

    0

--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -7,8 +7,8 @@
 Point makePoint( float x, float y ) {
     Point new;
     // implementation
-    new.x = 1.0;
-    new.y = 2.0;
+    new.x = x;
+    new.y = y;
     return new;
 }
 
@@ -19,5 +19,59 @@ Point makePoint( float x, float y ) {
 
 Line makeLine( Point p1, Point p2 ){
     Line l;
-    
+    l.p[0] = p1;
+    l.p[1] = p2;
+    return l;
 }
+
+Triangle makeTriangle( Point p1, Point p2, Point p3 ){
+    Triangle t;
+    t.p[0] = p1;
+    t.p[1] = p2;
+    t.p[2] = p3;
+    return t;
+}
+
+
+/*
+*Pythagorean theorem to find distance
+*/
+float lineLength( Line l ){
+    float length = (float)sqrt(pow((double)(l.p[0].x - l.p[1].x), 2) + pow((double)(l.p[0].y - l.p[1].y), 2));
+    return length;
+}
+
+/*
+*calculate the area by using cross product
+*/
+float triangleArea( Triangle t ){
+    float x1 = t.p[0].x;
+    float y1 = t.p[0].y;
+    float x2 = t.p[1].x;
+    float y2 = t.p[1].y;
+    float x3 = t.p[2].x;
+    float y3 = t.p[2].y;
+    float area = 0.5 * fabs((x2-x1)*(y3-y1)-(y2-y1)*(x3-x1));
+    return area;
+}
+
+bool samePoint( Point p1, Point p2 ){
+    makeLine( Point p1, Point p2 );
+    lineLength( Line l );
+    if(length<1.0e-6){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool pointInLine( Point p, Line l){
+    if(samePoint(Point p, Point l.p[0]) || samePoint(Point p, Point l.p[1])){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
